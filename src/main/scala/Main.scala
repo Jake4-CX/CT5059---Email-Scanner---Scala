797f574634933cd34f5bb5fc1ddc976799578e76
= <jake44264@hotmail.co.uk>
2023-03-14 00:00:49 +0000

    Current Progress

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,43 +1,103 @@
 package lat.jack.emailscanner
 
+import java.io.File
 import scala.collection.MapView
+import scala.io.Source
 
-object Main extends App {
-
+object Main {
   private val illegalPhrases: List[String] = List("threat", "vulnerabilities", "attack", "loopholes", "whitelist", "blacklist", "hash", "encryption")
-  private var emailInput: String = "You may seek him in the basement, you may look up in the air"
-  emailInput = "I have installed a malware onto their system, it will encrypt all their files by using that vulnerability that you have found. The attack will start on friday which will exploit the loopholes in their system. It will also wipe the whitelist and blacklist and their system will collapse."
+  private val minimumIllegalPhraseCount: Int = 5
 
-  emailInput = emailInput.toLowerCase() // Normalize the string - to lowercase
+  private def readFile(fileName: String): String = {
+    val file = Source.fromFile(fileName)
+    val fileContents = file.mkString
+    file.close()
+    fileContents
+  }
 
-  private val emailInputWords: Array[String] = emailInput.split(" ")
-
-  private val wordOccurrences: Seq[(String, Int)] = emailInputWords.groupMapReduce(identity)(_ => 1)(_ + _).toSeq.sortWith(_._2 > _._2)
-  // println(wordOccurrences)
-
-  private var illegalPhrasesUsed: Seq[(String, Int)] = Seq()
-  private var illegalPhraseCount: Int = 0
-
-  wordOccurrences.foreach((word, occurrence) => {
-    if (illegalPhrases.contains(word) && occurrence > 2) {
-      illegalPhrasesUsed :+ (word, occurrence)
-      illegalPhraseCount += occurrence
+  private def readDirectory(directoryPath: String): Array[String] = {
+    val directory = new File(directoryPath)
+    if (directory.exists && directory.isDirectory) {
+      directory.listFiles.filter(_.isFile).map(_.getName)
+    } else {
+      Array[String]()
     }
-  })
+  }
 
+  def main(args: Array[String]): Unit = {
+    if (args.length == 0) {
+      println("No parameters have been passed in. Please pass in either -F or -D")
+      println("For more information, please run the program with the -H parameter")
+    } else if (args.length == 1) {
+      if (args(0) == "-H") {
+        println("This program will scan an email for suspicious phrases")
+        println("To run the program, please pass in either -F or -D")
+        println("-F will scan a single file")
+        println("-D will scan all the files in a directory")
+      } else {
+        println("Please pass in either -F or -D")
+        println("For more information, please run the program with the -H parameter")
+      }
+    } else if (args.length == 2) {
+      if (args(0) == "-F") {
+        val fileContents = readFile(args(1))
+        scanEmail(fileContents)
+        // Main.main(Array())
+      } else if (args(0) == "-D") {
+        val files = readDirectory(args(1))
+        for (file <- files) {
+          val fileContents = readFile(args(1) + "/" + file)
+          scanEmail(fileContents)
+          // Main.main(Array())
+        }
+      } else {
+        println("Please pass in either -F or -D")
+        println("For more information, please run the program with the -H parameter")
+      }
+    } else {
+      println("Please pass in either -F or -D")
+      println("For more information, please run the program with the -H parameter")
+    }
+  }
 
-  println(s"Email: ${emailInput}")
-  // Print the list of illegal phrases used (key) from illegalPhrasesUsed
-  println("Phrases used: ")
-  illegalPhrasesUsed.foreach((word, occurrence) => {
-    println(word)
-  })
+  private def scanEmail(emailInput: String) = {
+    var email: String = emailInput
 
+    println("Scanning email...")
+    println(s"Email: ${email}")
 
-  if (wordOccurrences.length > 5 && illegalPhraseCount > 5) {
-    println("This is a Suspicious File")
-  } else {
-    println("This file is Benign")
+    email = email.toLowerCase() // Normalize the string - to lowercase
+
+    val emailInputWords: Array[String] = email.split(" ")
+
+    val wordOccurrences: Seq[(String, Int)] = emailInputWords.groupMapReduce(identity)(_ => 1)(_ + _).toSeq.sortWith(_._2 > _._2)
+
+    val illegalPhrasesUsed: Seq[(String, Int)] = wordOccurrences.filter(phrase => illegalPhrases.contains(phrase._1)) // min requirement = (phrase._2 >= 2)
+    val illegalPhraseCount: Int = illegalPhrasesUsed.map(_._2).sum
+
+    var build = ""
+    var i = 0
+
+    for (illegalPhrase <- illegalPhrasesUsed) {
+      build += illegalPhrase._1 + s" (${illegalPhrase._2})"
+
+      if (i < illegalPhrasesUsed.length - 1) {
+        build += ", "
+      } else build += "."
+
+      i += 1
+    }
+
+    println(s"Email: ${emailInput}")
+    println("")
+    println(s"Total Illegal Phrases Used: $illegalPhraseCount")
+    println(s"Illegal Phrases used $build")
+    println("")
+    if (wordOccurrences.length > 5 && illegalPhraseCount >= minimumIllegalPhraseCount) {
+      println("This is a Suspicious File")
+    } else {
+      println("This file is Benign")
+    }
   }
 
 }
